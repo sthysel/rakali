cd46fdcd44effc6a2f728aac7f32f47538bdbd58
Thys Meintjes <sthysel@gmail.com>
2019-02-14 12:47:55 +0800

    more wrappers

--- src/rakali/img.py
+++ src/rakali/img.py
@@ -61,6 +61,43 @@ class Image:
             inter=interpolation,
         )
 
+    def skeletonize(self, size: int, structuring: int = cv.MORPH_RECT):
+        """skeletonize image """
+        return imutils.skeletonize(
+            self.image,
+            size=size,
+            structuring=structuring,
+        )
+
+    def bgr2rgb(self):
+        """
+        OpenCV represents images in BGR order. Other libraries like Matplotlib
+        expects the image in RGB order
+        """
+        return cv.cvtColor(self.image, cv.COLOR_BGR2RGB)
+
+    def auto_canny(self, sigma=0.33):
+        """compute the median of the single channel pixel intensities """
+        return imutils.auto_canny(
+            image=self.image,
+            sigma=sigma,
+        )
+
+    def adjust_brightness_contrast(self, brightness: float = 0.0, contrast: float = 0.0, beta=0):
+        """
+        Adjust the brightness and/or contrast of an image
+        :param contrast: Float, contrast adjustment with 0 meaning no change
+        :param brightness: Float, brightness adjustment with 0 meaning no change
+        """
+
+        return cv.addWeighted(
+            src1=self.image,
+            alpha=1 + float(contrast) / 100.0,
+            src2=self.image,
+            beta=beta,
+            gamma=float(brightness),
+        )
+
     def show(self, wait=0, key='q', name='Image'):
         """display image"""
 
