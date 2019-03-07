f6c85db81bbbcb8a3c492703c5ef562d3d2cf8c3
Thys Meintjes <sthysel@gmail.com>
2019-03-07 23:03:45 +0800

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*names, **kwargs):
 setup(
     name='rakali',
     license='MIT',
-    version='0.0.2',
+    version='0.0.3',
     description='OpenCV Helper Tools',
     long_description=read('README.rst'),
     entry_points={
--- src/rakali/__init__.py
+++ src/rakali/__init__.py
@@ -2,4 +2,4 @@ from .video_reader import VideoStream
 from .video_writer import VideoWriter
 from .img import Image, ImageSize
 
-version = '0.0.2'
+version = '0.0.3'
