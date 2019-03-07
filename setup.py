440475f66ce1ab59c1bf19b149b10a1816088b08
Thys Meintjes <sthysel@gmail.com>
2019-03-07 23:30:51 +0800

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as f:
 setup(
     name='rakali',
     license='MIT',
-    version='0.0.3',
+    version='0.0.4',
     description='OpenCV Helper Tools',
     long_description=long_description,
     long_description_content_type='text/markdown',
