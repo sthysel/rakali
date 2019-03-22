0f9082a03185dfcc17f6e3c52fb39930544019fb
Thys Meintjes <sthysel@gmail.com>
2019-03-22 09:33:33 +0800

    dry out fish-eye calibration code

--- src/rakali/camera/fisheye.py
+++ src/rakali/camera/fisheye.py
@@ -2,14 +2,129 @@
 Fisheye camera support
 """
 
+import logging
+import sys
+from pathlib import Path
+from typing import Tuple
+
+import glob
 import cv2 as cv
 import numpy as np
-from rakali.video.fps import cost
-from typing import Tuple
-from pathlib import Path
 
-import logging
+from rakali.video.fps import cost
+
+from .chessboard import ChessboardFinder
+
 logger = logging.getLogger(__name__)
+
+CAL_FLAGS = cv.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv.fisheye.CALIB_CHECK_COND + cv.fisheye.CALIB_FIX_SKEW
+
+
+def get_zero_object(pattern_size=(9, 6), square_size=0.023):
+    rows, columns = pattern_size  # no
+    objp = np.zeros((1, rows * columns, 3), np.float32)
+    objp[0, :, :2] = np.mgrid[0:rows, 0:columns].T.reshape(-1, 2)
+    return objp
+
+
+def load_image_points_file(save_file) -> Tuple[list, list, tuple]:
+    """load from previously computed file """
+
+    try:
+        cache = np.load(save_file)
+        print(f'Loading previously computed image points from {save_file}')
+        return (
+            list(cache['object_points']),
+            list(cache['image_points']),
+            tuple(cache['image_size']),
+        )
+    except IOError:
+        print(f'{save_file} not found')
+        return None
+
+
+def save_image_points_file(
+    save_file,
+    object_points,
+    image_points,
+    image_size,
+):
+    np.savez_compressed(
+        save_file,
+        object_points=object_points,
+        image_points=image_points,
+        image_size=image_size,
+    )
+
+
+def get_points_from_chessboard_images(
+    boards_path,
+    chessboard_size,
+    square_size,
+):
+    """
+    Process folder with chesboard images and gather image points
+    """
+    print('Processing chessboard images...')
+
+    def check_size(image, image_size):
+        if image_size is None:
+            return img.shape[:2]
+        else:
+            if img.shape[:2] != image_size:
+                print(f'Image {fname} size incorrect')
+                sys.exit()
+        return image_size
+
+    image_size = None
+    images = glob.glob(str(boards_path / '*.jpg'))
+    zero = get_zero_object(
+        square_size=square_size,
+        pattern_size=chessboard_size,
+    )
+    finder = ChessboardFinder(chessboard_size)
+    image_points = []
+    object_points = []
+    for fname in images:
+        img = cv.imread(fname)
+        image_size = check_size(image=img, image_size=image_size)
+        ok, corners = finder.corners(img, fast=False)
+        if ok:
+            image_points.append(corners)
+            object_points.append(zero)
+        else:
+            print(f'Ignoring {fname}')
+
+    h, w = image_size
+    return object_points, image_points, (w, h)
+
+
+def calibrate(object_points, image_points, image_size):
+    """
+    Calibrate the pinhole camera using image points
+    """
+    obj_length = len(object_points)
+    print(f'Calibrating on {obj_length} objects...')
+
+    # zero holders
+    K = np.zeros((3, 3))
+    D = np.zeros((4, 1))
+    rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(obj_length)]
+    tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(obj_length)]
+
+    rms, _, _, _, _ = cv.fisheye.calibrate(
+        objectPoints=object_points,
+        imagePoints=image_points,
+        image_size=image_size,
+        K=K,
+        D=D,
+        rvecs=rvecs,
+        tvecs=tvecs,
+        flags=CAL_FLAGS,
+        criteria=(cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 1e-6),
+    )
+
+    return rms, K, D
 
 
 def save_calibration(
--- src/rakali/cli/calibrate_fisheye.py
+++ src/rakali/cli/calibrate_fisheye.py
@@ -5,180 +5,14 @@ Calibrate fisheye camera given set of chessboard images
 import click
 import sys
 import random
-import numpy as np
-import cv2 as cv
 from pathlib import Path
-import glob
 import logging
-from rakali.camera.chessboard import ChessboardFinder
 
-from rakali.camera.fisheye import save_calibration
-
-from typing import Tuple
+# from rakali.camera.fisheye import save_calibration
+from rakali.camera import fisheye
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-
-def get_zero_object(pattern_size=(9, 6), square_size=0.023):
-    rows, columns = pattern_size  # no
-    objp = np.zeros((1, rows * columns, 3), np.float32)
-    objp[0, :, :2] = np.mgrid[0:rows, 0:columns].T.reshape(-1, 2)
-    return objp
-
-
-def save_image_points_file(
-    save_file,
-    object_points,
-    image_points,
-    image_size,
-):
-    np.savez_compressed(
-        save_file,
-        object_points=object_points,
-        image_points=image_points,
-        image_size=image_size,
-    )
-
-
-def load_image_points_file(save_file) -> Tuple[list, list, tuple]:
-    """load from previously computed file """
-
-    try:
-        cache = np.load(save_file)
-        print(f'Loading previously computed image points from {save_file}')
-        return (
-            list(cache['object_points']),
-            list(cache['image_points']),
-            tuple(cache['image_size']),
-        )
-    except IOError:
-        print(f'{save_file} not found')
-        return None
-
-
-def get_points_from_chessboard_images(boards_path, chessboard_size, square_size):
-    """
-    Process folder with chesboard images and gather image points
-    """
-    print('Processing chessboard images...')
-
-    def check_size(image, image_size):
-        if image_size is None:
-            return img.shape[:2]
-        else:
-            if img.shape[:2] != image_size:
-                print(f'Image {fname} size incorrect')
-                sys.exit()
-        return image_size
-
-    image_size = None
-    images = glob.glob(str(boards_path / '*.jpg'))
-    zero = get_zero_object(
-        square_size=square_size,
-        pattern_size=chessboard_size,
-    )
-    finder = ChessboardFinder(chessboard_size)
-    image_points = []
-    object_points = []
-    for fname in images:
-        img = cv.imread(fname)
-        image_size = check_size(image=img, image_size=image_size)
-        ok, corners = finder.corners(img, fast=False)
-        if ok:
-            image_points.append(corners)
-            object_points.append(zero)
-        else:
-            print(f'Ignoring {fname}')
-
-    h, w = image_size
-    return object_points, image_points, (w, h)
-
-
-def calibrate(object_points, image_points, image_size):
-    """
-    Calibrate the pinhole camera using image points
-    """
-    obj_length = len(object_points)
-    print(f'Calibrating on {obj_length} objects...')
-
-    calibration_flags = cv.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv.fisheye.CALIB_CHECK_COND + cv.fisheye.CALIB_FIX_SKEW
-    # zero holders
-    K = np.zeros((3, 3))
-    D = np.zeros((4, 1))
-    rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(obj_length)]
-    tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(obj_length)]
-
-    rms, _, _, _, _ = cv.fisheye.calibrate(
-        objectPoints=object_points,
-        imagePoints=image_points,
-        image_size=image_size,
-        K=K,
-        D=D,
-        rvecs=rvecs,
-        tvecs=tvecs,
-        flags=calibration_flags,
-        criteria=(cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 1e-6),
-    )
-
-    return rms, K, D
-
-
-def do_calibrate(
-    input_folder,
-    chessboard_size,
-    square_size,
-    calibration_file,
-    image_points_file,
-    salt=888,
-    pick_size=50,
-):
-    # use previously computed image points if they are available
-    exiting_points = load_image_points_file(image_points_file)
-    if exiting_points:
-        object_points, image_points, image_size = exiting_points
-    else:
-        object_points, image_points, image_size = get_points_from_chessboard_images(
-            boards_path=input_folder,
-            chessboard_size=chessboard_size,
-            square_size=square_size,
-        )
-        save_image_points_file(
-            save_file=image_points_file,
-            object_points=object_points,
-            image_points=image_points,
-            image_size=image_size,
-        )
-
-    w, h = image_size
-    assert (w > h)
-
-    # reduce points list else calibration takes too long
-    random.seed(salt)
-    image_points = random.choices(image_points, k=pick_size)
-    object_points = object_points[:pick_size]
-
-    rms, K, D = calibrate(
-        object_points=object_points,
-        image_points=image_points,
-        image_size=image_size,
-    )
-
-    save_calibration(
-        calibration_file,
-        K=K,
-        D=D,
-        image_size=image_size,
-        salt=salt,
-        pick_size=pick_size,
-        error=rms,
-    )
-
-    print(f'DIM={image_size}')
-    print(f'K=np.array({str(K.tolist())})')
-    print(f'D=np.array({str(D.tolist())})')
-
-    return rms
 
 
 @click.command(context_settings=dict(max_content_width=120))
@@ -243,8 +77,9 @@ def cli(
     pick_size,
 ):
     """
-    Calibrate pinhole camera using chessboard frames captured earlier.
+    Calibrate fish-eye camera using chessboard frames captured earlier.
     """
+
     if pick_size < 5:
         print(f'A set of {pick_size} is to small')
         sys.exit()
@@ -254,13 +89,50 @@ def cli(
         click.secho(message=f'Folder {input_folder} does not exist', err=True)
         sys.exit()
 
-    error = do_calibrate(
-        input_folder=input_folder,
-        chessboard_size=(chessboard_columns, chessboard_rows),
-        square_size=square_size,
-        calibration_file=calibration_file,
-        image_points_file=image_points_file,
+    chessboard_size = (chessboard_columns, chessboard_rows)
+
+    # use previously computed image points if they are available
+    exiting_points = fisheye.load_image_points_file(image_points_file)
+    if exiting_points:
+        object_points, image_points, image_size = exiting_points
+    else:
+        object_points, image_points, image_size = fisheye.get_points_from_chessboard_images(
+            boards_path=input_folder,
+            chessboard_size=chessboard_size,
+            square_size=square_size,
+        )
+        fisheye.save_image_points_file(
+            save_file=image_points_file,
+            object_points=object_points,
+            image_points=image_points,
+            image_size=image_size,
+        )
+
+    w, h = image_size
+    assert (w > h)
+
+    # reduce points list else calibration takes too long
+    random.seed(salt)
+    image_points = random.choices(image_points, k=pick_size)
+    object_points = object_points[:pick_size]
+
+    rms, K, D = fisheye.calibrate(
+        object_points=object_points,
+        image_points=image_points,
+        image_size=image_size,
+    )
+
+    fisheye.save_calibration(
+        calibration_file,
+        K=K,
+        D=D,
+        image_size=image_size,
         salt=salt,
         pick_size=pick_size,
+        error=rms,
     )
-    click.secho(message=f'Calibration error: {error}')
+
+    print(f'DIM={image_size}')
+    print(f'K=np.array({str(K.tolist())})')
+    print(f'D=np.array({str(D.tolist())})')
+    click.secho(message=f'Calibration error: {rms}')
