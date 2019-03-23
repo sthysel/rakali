4f46b139479a908ff1ca679c27cccb6596cf1089
Thys Meintjes <sthysel@gmail.com>
2019-03-23 08:35:54 +0800

    dry up pinhole

--- src/rakali/camera/pinhole.py
+++ src/rakali/camera/pinhole.py
@@ -2,96 +2,24 @@
 Support for pinhole camera
 """
 
-from rakali.video.fps import cost
-import sys
-import numpy as np
-import cv2 as cv
-import glob
 import logging
+import time
 from pathlib import Path
-from rakali.camera.chessboard import ChessboardFinder
+
+import cv2 as cv
+import numpy as np
+
+from rakali.video.fps import cost
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def get_zero_object(pattern_size=(9, 6), square_size=0.023):
+def xxx_get_zero_object(pattern_size=(9, 6), square_size=0.023):
     pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)
     pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
     pattern_points *= square_size
     return pattern_points
-
-
-def load_image_points_file(save_file):
-    """load from previously computed file """
-
-    try:
-        cache = np.load(save_file)
-        logger.debug(f'Loading previously computed image points from {save_file}')
-        return (
-            list(cache['object_points']),
-            list(cache['image_points']),
-            tuple(cache['image_size']),
-        )
-    except IOError:
-        logger.warning(f"Cache file at {save_file} not found")
-        return None
-
-
-def save_image_points_file(
-    save_file,
-    object_points,
-    image_points,
-    image_size,
-):
-    np.savez_compressed(
-        save_file,
-        object_points=object_points,
-        image_points=image_points,
-        image_size=image_size,
-    )
-
-
-def get_points_from_chessboard_images(
-    boards_path,
-    chessboard_size,
-    square_size,
-):
-    """
-    Process folder with chesboard images and gather image points
-    """
-    logging.debug('Processing chessboard images...')
-
-    def check_size(image, image_size):
-        if image_size is None:
-            return img.shape[:2]
-        else:
-            if img.shape[:2] != image_size:
-                print(f'Image {fname} size incorrect')
-                sys.exit()
-        return image_size
-
-    image_size = None
-    images = glob.glob(str(boards_path / '*.jpg'))
-    zero = get_zero_object(
-        square_size=square_size,
-        pattern_size=chessboard_size,
-    )
-    finder = ChessboardFinder(chessboard_size)
-    image_points = []
-    object_points = []
-    for fname in images:
-        img = cv.imread(fname)
-        image_size = check_size(image=img, image_size=image_size)
-        ok, corners = finder.corners(img, fast=False)
-        if ok:
-            image_points.append(corners)
-            object_points.append(zero)
-        else:
-            logger.info(f'Ignoring {fname}')
-
-    h, w = image_size
-    return object_points, image_points, (w, h)
 
 
 def calibrate(object_points, image_points, image_size):
@@ -147,9 +75,10 @@ def save_calibration(
     distortion_coefficients,
     rotation,
     translation,
-    salt,
-    pick_size,
-    error,
+    salt: int,
+    pick_size: int,
+    error: float,
+    cid: str,
 ):
     """Save pinhole calibration to file """
     np.savez_compressed(
@@ -163,6 +92,8 @@ def save_calibration(
         salt=salt,
         pick_size=pick_size,
         error=error,
+        cid=cid,
+        time=time.time(),
     )
 
 
@@ -180,6 +111,8 @@ def load_calibration(calibration_file):
         salt=int(cal['salt']),
         pick_size=int(cal['pick_size']),
         error=float(cal['error']),
+        cid=str(cal['cid']),
+        time=float(cal['time']),
     )
 
 
@@ -208,6 +141,27 @@ class CalibratedPinholeCamera:
             self.calibration = load_calibration(calibration_file=calibration_file)
         else:
             logger.error(f'Calibration file {calibration_file} does not exist')
+
+    def set_calibration(self, calibration):
+        """set calibration"""
+        # TODO validate
+        self.calibration = calibration
+
+    @property
+    def cid(self):
+        """calibration id"""
+        if self.calibration:
+            return self.calibration.get('cid', 'UNSET')
+        else:
+            return 'UNSET'
+
+    @property
+    def calibration_time(self):
+        """calibration time"""
+        if self.calibration:
+            return self.calibration.get('time', -1)
+        else:
+            return -1
 
     @cost
     def correct(self, frame):
