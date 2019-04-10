7e4623cc82bd028d7f8063ce5b14f35df37fe134
Thys Meintjes <sthysel@gmail.com>
2019-04-10 17:10:13 +0800

    some import guards

--- src/rakali/__init__.py
+++ src/rakali/__init__.py
@@ -2,6 +2,25 @@
 Rakali image processing
 """
 
+advice = """
+OpenCV needs to be installed.
+Many users use opencv-cuda or some custom OpenCV built and do not appreciate that
+being clobbered.
+
+For standard usuage just install from pip:
+$ pip install opencv-python
+
+For arch-linux use the opencv-cuda pac from AUR:
+$ yay -S opencv-cuda
+"""
+
+import sys
+try:
+    import cv2
+except ImportError:
+    print(advice)
+    sys.exit()
+
 version = '0.0.9'
 
 from .video.reader import VideoStream
